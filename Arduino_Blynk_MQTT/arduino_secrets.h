ebbe811a8c3a25a49d0e2f3dc1bd7c75c6e834af
El-Tomaz <tomazlemosgarcia@gmail.com>
2025-09-04 23:43:18 -0300

    Adicionando Blynk, amanha testo direitinho!

---
+++ Arduino_Blynk_MQTT/arduino_secrets.h
@@ -0,0 +1,17 @@
+
+/* WiFi credentials. Set password to "" for open networks. */
+#define WIFI_SSID                 "espwifi"
+#define WIFI_PASS                 "tomaz123"
+
+/* Fill in information from Blynk Device Info here.
+   Read More: https://bit.ly/BlynkSimpleAuth
+*/
+#define BLYNK_TEMPLATE_ID "TMPL2UiUU8nzW"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "XJgInAnRo7Ao1l7jYcwG5x__azBQtA-Z"
+/* Change the default Blynk server. Applicable for users with a white label plan. */
+//#define BLYNK_MQTT_BROKER       "blynk.cloud"
+//#define BLYNK_MQTT_PORT         8883
+
+/* Disable Secure TLS connection. Only use it for testing purposes. */
+//#define BLYNK_MQTT_UNSECURE
