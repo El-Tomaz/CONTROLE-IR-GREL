fbe62aa6347ef7dde4ba7c4db5cf211bb6938295
El-Tomaz <tomazlemosgarcia@gmail.com>
2025-09-14 12:08:10 -0300

    dev: botoes implementados, uplink para blynk mqtt implementado
    todo: downlink do blynk mqtt (o controle local terá prioridade)
    todo: pcb
    todo: case 3d

--- Arduino_Blynk_MQTT/Arduino_Blynk_MQTT.ino
+++ Arduino_Blynk_MQTT/Arduino_Blynk_MQTT.ino
@@ -11,6 +11,15 @@
 #include <WiFiManager.h>
 
 
+// ========= HARDWARE MAPPING! ===========
+const int pButtons[3] = {0,4,5}; //on/off up down
+// ATENCAO: os numeros dos pinos na placa esta diferente, conecte fisicamente aos pinos: 3 2 1
+
+// ========= systems constraints
+#define MAX_TEMP 24
+#define MIN_TEMP 18
+
+
 
 // These commands are executed every time
 // the device (re)connects to the Blynk Cloud
@@ -59,15 +68,84 @@ void setup()
 
   systemShowDeviceInfo();
   setClock();
+
+  
+  for(int i = 0; i < 3; i++)pinMode(pButtons[i], INPUT_PULLUP);
+
 }
+
+
 
 void loop()
 {
+  static struct {
+    int temp = 20;
+    bool on = true;
+  } status;
 
+  bool button_pressed = false; //flag to just send commando to ac and to blynk when button pressed
+  static int last_button_state[3] = {1}; // on/off up down
+  int actual_button_state[3] = {1};
+
+  for(int i = 0; i < 3;i++){
+    actual_button_state[i] = digitalRead(pButtons[i]);
+    
+    //detectando borda de descida
+    if(last_button_state[i] == 1 && actual_button_state[i] == 0){
+        
+        button_pressed = true;
+
+        switch(i){
+          // on/off
+          case 0:
+            status.on = !status.on;
+          break;
+
+          // up
+          case 1:
+            status.temp++;
+          break;
+
+          //down
+          case 2:
+            status.temp--;
+          break;
+
+        }
+    }
+
+    last_button_state[i] = actual_button_state[i];  
+  }
+
+  // tratando dados (evitar limite de temperatura)
+  if(status.temp > MAX_TEMP){
+    status.temp = MAX_TEMP;
+  }
+  else if(status.temp < MIN_TEMP){
+    status.temp = MIN_TEMP;
+  }
+
+  //enviando commando 
+  if(button_pressed){
+    Serial.println("BUtton pressed!");
+    Serial.println(status.temp);
+    Serial.println(status.on);
+    // send commando to ac
+  }
+
+  // sending info to blynk
+  if(button_pressed){
+    char temp_payload[3] = "00";
+    sprintf(temp_payload, "%d", status.temp);
+    mqtt.publish("ds/temp", temp_payload);
+
+    char on_off[2] = "";
+    on_off[0] = status.on == true? '1':'0';
+    mqtt.publish("ds/on", on_off);
+
+  }
 
   // Keep WiFi and MQTT connection
-  
-
   if (!mqtt.connected()) {
     connectMQTT();
   } else {
--- Arduino_Blynk_MQTT/arduino_secrets.h
+++ Arduino_Blynk_MQTT/arduino_secrets.h
@@ -1,10 +1,11 @@
 
+
 /* Fill in information from Blynk Device Info here.
    Read More: https://bit.ly/BlynkSimpleAuth
 */
-#define BLYNK_TEMPLATE_ID "TMPL2UiUU8nzW"
-#define BLYNK_TEMPLATE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN "XJgInAnRo7Ao1l7jYcwG5x__azBQtA-Z"
+#define BLYNK_TEMPLATE_ID "TMPL2x7YYtIFc"
+#define BLYNK_TEMPLATE_NAME "Controle GREL"
+#define BLYNK_AUTH_TOKEN "yMd0juu33lNdFQ_nCQVAi1ru5th6HobZ"
 /* Change the default Blynk server. Applicable for users with a white label plan. */
 //#define BLYNK_MQTT_BROKER       "blynk.cloud"
 //#define BLYNK_MQTT_PORT         8883
